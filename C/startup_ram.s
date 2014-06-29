bfc61b3d687a32ec7008d6f75d42f189b419fdc2
zigzagjoe <zigzagjoe@gmail.com>
2014-06-28 20:26:07 -0500

    De-tabify startup scripts, minor commenting, standardize link script stack size and fix ram overridable stack size

--- C/startup_ram.s
+++ C/startup_ram.s
@@ -1,4 +1,4 @@
-.align	2
+.align    2
 .text
 
 .global _start
@@ -13,28 +13,27 @@
 .set TIL311, IO_BASE + 0x8000
 
  _start:
-	move.b #0xCC, (TIL311)        | what up my glip glops!
-	move.l #0xFFFFFFFF, (0x400) 
-	                              | kernel not active
-	
-	move.l #__stack_start, %a7
+    move.b #0xCC, (TIL311)        | what up my glip glops!
+    move.l #0xFFFFFFFF, (0x400)   | mark kernel not active
+    
+    move.l #__stack_start, %a7
 
-	/* clear BSS section */
+    /* clear BSS section */
 
-	move.l #__bss_size, %d0
-	
-	cmp.l #0, %d0  	              | check if empty bss section
-	beq run
-	
-	move.l #__bss_start, %a0 	  | A0 = _bss start
-	
-	| due to use of dbra, we can clear a max of 256kb of RAM (65536 * sizeof(long))
+    move.l #__bss_size, %d0
+    
+    cmp.l #0, %d0                 | check if empty bss section
+    beq run
+        
+    move.l #__bss_start, %a0      | A0 = _bss start
+    
+    | due to use of dbra, we can clear a max of 256kb of RAM (65536 * sizeof(long))
 cbss:
-	clr.l (%a0)+                  | clear [A0.l]
-	dbra %d0, cbss 		          | D0 != 0
-	 
+    clr.l (%a0)+                  | clear [A0.l]
+    dbra %d0, cbss                | D0 != 0
+     
 run:
-	jsr __start_func              | jump to main
-	
+    jsr __start_func              | jump to main
+    
 _exit:                            | loop forever
-	bra _exit                     | could return to monitor if present
+    bra _exit                     | could return to monitor if present
--- C/startup_rom.s
+++ C/startup_rom.s
@@ -1,4 +1,4 @@
-.align	2
+.align   2
 .text
 
 .global _start
@@ -19,45 +19,45 @@
 .long 0xc141c340
 
 _start:
-	move.b #0xCC, (TIL311)         | what up my glip glops!
+   move.b #0xCC, (TIL311)         | what up my glip glops!
 
-	move.l #0xFFFFFFFF, (0x400)    | kernel not active
-	
-	move.l #__stack_start, %a7
-	
-	/* copy data */
-	
-	move.l #__data_size, %d0	
+   move.l #0xFFFFFFFF, (0x400)    | mark kernel not active
+   
+   move.l #__stack_start, %a7
+   
+   /* copy writeable data section into RAM */
+   
+   move.l #__data_size, %d0   
 
-	cmp.l #0, %d0  	               | check if empty data section
-	beq clrbss
+   cmp.l #0, %d0                    | check if empty data section
+   beq clrbss
 
-	move.l #__data_rom_start, %a0  | A0 = data start
-	move.l #0x2000, %a1            | A1 = data dest
-	
-	
-	| due to use of dbra, we can copy a max of 256kb
+   move.l #__data_rom_start, %a0   | A0 = data start
+   move.l #__data_ram_start, %a1   | A1 = data dest
+   
+   | due to use of dbra, we can copy a max of 256kb - more than enough
 cpydat:
-	move.l (%a0)+, (%a1)+          | *A1++ = *A0++
-	dbra %d0, cpydat               | D0 != 0
+   move.l (%a0)+, (%a1)+           | *A1++ = *A0++
+   dbra %d0, cpydat                | D0 != 0
 
 clrbss:
-	/* clear BSS section */
-	
-	move.l #__bss_size, %d0
-	
-	cmp.l #0, %d0  	               | check for empty bss section
-	beq run
-	
-	move.l #__bss_start, %a0 	   | A0 = _bss start
-	
-	| due to use of dbra, we can clear a max of 256kb of RAM (65536 * sizeof(long))
+   /* clear BSS section */
+   
+   move.l #__bss_size, %d0
+   
+   cmp.l #0, %d0                   | check for empty bss section
+   beq run_main
+   
+   move.l #__bss_start, %a0        | A0 = _bss start
+   
+   | due to use of dbra, we can clear a max of 256kb of RAM (65536 * sizeof(long))
 cbss:
-	clr.l (%a0)+                   | clear [A0.l]
-	dbra %d0, cbss 		           | D0 != 0
-	
-run:
-	jsr __start_func               | jump to main
-	
+   clr.l (%a0)+                    | clear [A0.l]
+   dbra %d0, cbss                  | D0 != 0
+   
+   /* ready to run c code */
+run_main:
+   jsr __start_func                | jump to main
+   
 _exit:                             | loop forever
-	bra _exit                      | could return to monitor if present
+   bra _exit                       | could/should return to monitor if present
