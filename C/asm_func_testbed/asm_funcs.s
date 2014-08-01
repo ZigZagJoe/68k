996acd3eec2acd269c193d8c85a2278482a544ff
zigzagjoe <zigzagjoe@gmail.com>
2014-07-31 23:12:00 -0500

    Sync rest of shit

--- C/asm_func_testbed/asm_funcs.s
+++ C/asm_func_testbed/asm_funcs.s
@@ -3,142 +3,34 @@
 
 .extern putc
 
-.global do_test1
-.global do_test2
+
+.global _strlen
+.global _nulltest
 
 | void *memcpy(void *dst, const void *src, size_t len) (return *dst)
 .extern memcpy
 .extern millis_counter
 
-do_test1:
-    movem.l %d2-%d7/%a2-%a6, -(%sp)
+#uint16_t _strlen(char* str);
+   
+_strlen:
+    move.l 4(%sp), %a1   | dst
+	move.w #-1, %d0
+	
+_cnt:
+	tst.b (%a1)+
+	dbeq %d0, _cnt
+	
+	addq.w #1, %d0
+	neg.w %d0
 
-    | copy 192 kb
-    move.l #131072, -(%sp)
-    move.l #0, -(%sp)
-    move.l #262144, -(%sp)
-    move.l (millis_counter), %d2
-    
-    jsr memcpy
-    jsr memcpy
-    jsr memcpy
-    jsr memcpy
-    jsr memcpy
-    jsr memcpy
-    jsr memcpy
-    jsr memcpy          
-    
-    move.l (millis_counter), %d3
-    
-    add.l #12, %sp
-    
-    sub.l %d2, %d3
-    move.l %d3, %d0
-    
-    movem.l (%sp)+, %d2-%d7/%a2-%a6
     rts
     
-do_test2:
-    movem.l %d2-%d7/%a2-%a6, -(%sp)
-
-    | copy 192 kb
-    move.l #131072, -(%sp)
-    move.l #0, -(%sp)
-    move.l #262144, -(%sp)
-    move.l (millis_counter), %d2
-    
-    jsr memcpy2
-    jsr memcpy2
-    jsr memcpy2
-    jsr memcpy2
-    jsr memcpy2
-    jsr memcpy2
-    jsr memcpy2
-    jsr memcpy2         
-    
-    move.l (millis_counter), %d3
-    
-    add.l #12, %sp
-    
-    sub.l %d2, %d3
-    move.l %d3, %d0
-    
-    movem.l (%sp)+, %d2-%d7/%a2-%a6
+_nulltest:
+     move.l 4(%sp), %d0   | dst
+	clr.l %d0
+	
     rts
-    
-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-| void *memcpy(void *dst, const void *src, size_t len) (return *dst)
-memcpy2:
-	move.l 4(%sp), %a1   | dst
-	move.l 8(%sp), %a0   | src
-	move.l 12(%sp), %d0  | count
-	move.w %d2, -(%sp)
-	
-	tst.l %d0            | nope
-	beq _endcpy
-	
-	cmp.l #5, %d0		 | long routine does not work below 5 bytes, use byte copy
-	blt _bytememc
-	
-	move.w %a1, %d1      | verify alignment between src and dest
-	move.w %a0, %d2
-	andi.b #1, %d1
-	andi.b #1, %d2
-	
-	cmp.b %d1, %d2       | if the alignment differs, this /has/ to be a byte copy
-	jne _bytememc
-	
-	tst.b %d1
-	beq _aligned_cpy     | if lowest bit is not set, we are aligned
-	
-	move.b (%a0)+,(%a1)+ | copy first byte so that we are aligned
-	subi.l #1, %d0
-	
-_aligned_cpy:
-	move.b %d0, %d1      | save for later
-	divu #48, %d0        | figure out block count
-	swap %d0
-	move.w %d0, %d1      | save remainder to copy afterwards
-	swap %d0
-	
-	sub.w #1, %d0        | compensate dbra -1 requirement
-	
-	movem.l %d1-%d7/%a2-%a6, -(%sp)
-    
-_qcpy:
-	movem.l (%a0)+, %d1-%d7/%a2-%a6
-    movem.l %d1-%d7/%a2-%a6, (%a1)
-    add #(12*4), %a1
-    dbra %d0, _qcpy
-	
-	movem.l (%sp)+, %d1-%d7/%a2-%a6
-    
-    beq _endcpy
-
-	subi.w #1, %d1       | dbra -1
-	
-_oddcpy:	
-	move.b (%a0)+,(%a1)+ | byte copy
-	dbra %d1, _oddcpy
-	
-_endcpy:
-	move.w (%sp)+, %d2
-	move.l 4(%sp),%d0
-	rts
-	
-	| byte copy routine for unaligned copy and small copy
-	| seperate because it may be neccisary to copy in excess of 64k bytes unaligned
-_bytememc:
-	sub.l #1, %d0
-
-_lbcpy:
-	move.b (%a0)+, (%a1)+
-	subi.l #1, %d0
-	bpl _lbcpy
-	
-	bra _endcpy
-
-
 
 | put hex long from D0
 puthexlong:
--- C/asm_func_testbed/asm_test.c
+++ C/asm_func_testbed/asm_test.c
@@ -10,15 +10,8 @@
 #include <lcd.h>
 #include <time.h>
 
-void simp_printf(char * str, ...);
 
-uint32_t do_test1();
-uint32_t do_test2();
-
-uint32_t crc_region (uint8_t * start, uint32_t sz) ;
-void test(uint32_t src, uint32_t dest, uint32_t len) ;
-uint32_t hash( uint32_t a);
-void mem_dump(uint8_t *addr, uint32_t cnt);
+uint16_t _strlen(char* str);
 
 int main() {
     TIL311 = 0x01;
@@ -26,7 +19,39 @@ int main() {
 	serial_start(SERIAL_SAFE);
     millis_start();
     sei();
+    char * test_str = "hello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lolhello world what what in the butt lol";
     
+    uint32_t start, end;
+    int ret;
+    
+    start = millis();
+    for (int i = 0; i < 100; i++)
+        ret = strlen(test_str);
+        
+    end = millis();
+        
+    printf("Str len: %d\n",ret);
+     printf("Time: %d\n", end-start);
+
+    start = millis();
+    for (int i = 0; i < 100; i++)
+        ret = _strlen(test_str);
+    end = millis();
+    
+    printf("Str len2: %d\n", ret);
+    printf("Time: %d\n", end-start);
+    
+    
+    printf("Null test\n");
+    start = millis();
+    for (int i = 0; i < 100; i++)
+        ret = _nulltest(test_str);
+    end = millis();
+    
+    printf("Time: %d\n", end-start);
+     
+    DELAY_MS(250);
+    __asm ("move.w #0xB007, 0x400\njmp 0x80008\n");
     //srand();
     
     
@@ -36,142 +61,4 @@ int main() {
     
     t = do_test2();
     printf("Cpy 2: %d\n", t * 10);*/
-    
-    uint32_t ha = 124912341;
-    uint8_t c = 0;
-	for (uint32_t i = 0x10000; i < 0x20100; i += 4) {
-		*((uint32_t*)(i)) = ha;
-		ha = hash(ha + i);
-		//*((uint8_t*)(i)) = c++;
-		//*((uint8_t*)(i + 0x20000)) = c++;
-	}
-	
-	uint32_t start, end;
-	
-	puts("Source\n");
-	mem_dump(0x10000, 256);
-	mem_dump(0x1FF00, 384);
-	
-	puts("Dest org\n");
-	mem_dump(0x30000, 256);
-	mem_dump(0x3FF00, 384);
-	
-	start = millis();
-	memset(0x30000, 'C', 0x10000);
-	end = millis();
-	
-	memset(0x40000, '#', 128);
-	
-	printf("Clear done in %d\n", end-start);
-	
-	puts("Dest clr\n");
-	mem_dump(0x30000, 256);
-	mem_dump(0x3FF00, 384);
-	
-	start = millis();
-	memcpy(0x30000,0x10000,0x10000);
-	end = millis();
-	
-	printf("Copy done in %d\n", end-start);
-	
-    puts("After copy\n");
-	mem_dump(0x30000, 256);
-	mem_dump(0x3FF00, 384);
-	
-	test(0x10000, 0x30000, 10);
-    test(0x10000, 0x31000, 100);
-	test(0x10000, 0x30300, 1337);
-	test(0x100e1, 0x30100, 1337);
-	test(0x100e0, 0x30001, 213);
-	test(0x100e1, 0x30001, 123);
-	test(0x10000, 0x30000, 3);
-	test(0x10011, 0x30010, 1);
-	test(0x10000, 0x30011, 1);
-	test(0x10009, 0x30011, 4);
-	
-    while(true);
-    
-   /*
-   char * str2 = "embedded str";
-	simp_printf("hello world\nhex= 0x%x\nstring= %s\nbinary= %b\npercent %%\nchar= '%c'\nu_dec= '%u'\nu_dec_oor= '%u'\n", 0xDEADBEEF, str2, 0xCAFEBABE, 'h',132874,655369);
-	
-	simp_printf("s_dec= '%d'\n-s_dec= '%d'\ns_dec_oor= '%d'\n-s_dec_oor= '%d'\n", 21345,-10982,695369,-655600);
-	*/
-	
-	while(true)
-	    printf("%08x\n",rand());
-	
- 	
-}	
-	
-
-void mem_dump(uint8_t *addr, uint32_t cnt) {
-    int c = 0;
-    char ascii[17];
-    ascii[16] = 0;
-    
-    printf("\n%8X   ", addr);
-    for (uint32_t i = 0; i < cnt; i++) {
-        uint8_t b = *addr;
-        
-        ascii[c] = (b > 31 & b < 127) ? b : '.';
-        
-        printf("%02X ",b);
-        
-        addr++;
-        
-        if (++c == 16) {
-            printf("  %s\n",ascii);
-            if ((i+1) < cnt)
-                printf("%8X   ", addr);
-            c = 0;
-        }
-    }
-    
-    if (c && c < 15) {
-        ascii[c] = 0; // null terminate
-        while (c++ < 16)
-            printf("   ");
-        printf("  %s\n",ascii);
-    } else printf("\n");
 }
-
-uint32_t hash( uint32_t a) {
-    a = (a ^ 61) ^ (a >> 16);
-    a = a + (a << 3);
-    a = a ^ (a >> 4);
-    a = a * 0x27d4eb2d;
-    a = a ^ (a >> 15);
-    return a;
-}
-
-
-uint32_t crc_region (uint8_t * start, uint32_t sz) {
-	uint32_t crc = 0xDEADC0DE;
-	
-	for (uint32_t i = 0; i < sz; i++)
-		crc = qcrc_update(crc, start[i]);
-		
-	printf("QCRC of %d bytes from 0x%x: 0x%X\n", sz,start, crc);
-	return crc;
-}
-
-	
-void test(uint32_t src, uint32_t dest, uint32_t len) {
-	uint32_t start, end;
-	
-	memset(dest, 0, len);
-	start = millis();
-	memcpy(dest, src, len);
-	end = millis();
-	
-	printf("\nCopy: %d elapsed\n", end-start);
-	
-	start = millis();
-	if (crc_region(src, len) != crc_region(dest, len)) 
-		printf("ERROR: CRC MISMATCH!\n");
-	end = millis();
-	
-	printf("QCRC: %d elapsed\n", end-start);
-}
-
