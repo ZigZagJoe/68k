f5b9f94f9074963f8f405a5e144b748887eb68a7
zigzagjoe <zigzagjoe@gmail.com>
2014-08-05 00:04:05 -0500

    Am are idiot RE: kernel.s. move semaphore.s into libkernel.

--- C/libkernel/kernel.s
+++ C/libkernel/kernel.s
@@ -236,11 +236,12 @@ _swap_task:
 | find the next runnable task and run it 
 _run_next_task:
     move.l 4(%a0), %a0                  | node = node->next
-
+    
     tst.b 3(%a0)                        | is it in sleep?
     jpl _run_task                       | positive = uppermost bit not set = not sleep
         
-    cmp.l 8(%a0), (millis_counter)                   
+    move.l (millis_counter), %d7        
+    cmp.l 8(%a0), %d7                   
     jls _run_next_task                  | not time for this process yet, next!
     
     move.b #1, 3(%a0)                   | it is time: mark it as runnable
@@ -321,7 +322,7 @@ _sleep_for:
  _sleep_short:   
     sei 
     jra _user_swap
-
+    
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||    
 | create a new task
 | %a0: new task entry point
