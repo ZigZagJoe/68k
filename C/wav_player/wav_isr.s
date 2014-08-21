8f059cf83ccd957207b1d2d983c1676356239b1b
zigzagjoe <zigzagjoe@gmail.com>
2014-08-21 14:24:10 -0500

    Various tweaks to wav player...

--- C/wav_player/wav_isr.s
+++ C/wav_player/wav_isr.s
@@ -4,21 +4,69 @@
 .global wav_isr
 .global wav_ptr
 .global wav_end
+.global wav_semaphore
 
-.set TIL311, 0xC8000           | TIL311 address
+.set IO_BASE,  0xC0000
+.set MFP,      IO_BASE
+.set TIL311,   IO_BASE + 0x8000
+
+| MFP registers
+.set GPDR,     MFP+0x01 | GPIO data reg
+.set DDR,      MFP+0x05 | data direction
+.set IERB,     MFP+0x09 | interrupt enable B
+.set IMRB,     MFP+0x15 | interrupt mask B
+
+.macro sei
+    and.w #0xF8FF, %SR     
+.endm
 
 wav_isr:
-    move.l %a0, -(%sp)
+    | sei                    | allow interrupts
+    
+    tas.b (wav_semaphore)
+    jne wav_end
+    
+    move.b #2, (0xC0001)     | LED ON
+     
+    movem.l %d0/%a0-%a2, -(%sp)
+    
     move.l (wav_ptr), %a0
-    cmp.l (wav_end), %a0
-    jeq skip_wav
-    move.b (%a0)+, (TIL311)
+    cmp.l (wav_end), %a0     | perform bounds check
+    jhi skip_load            | at/beyond specified end point: do not load data
+    
+    move.l #TIL311, %a1      | load address; saves time in loops
+    move.l #GPDR, %a2        | bounds check address
+    
+      
+    | stage one: load bytes until half full is asserted
+    moveq #127, %d0
+
+bc_loop:
+    move.l (%a0)+, (%a1)
+    btst.b #2, (%a2)
+    dbeq %d0, bc_loop        | exit loop if GPDR bit 2 is 0
+    
+    | stage two: load no more than 256 bytes - 3 bytes (worst case)
+    moveq #62, %d0
+    
+rem_loop:
+    move.l (%a0)+, (%a1)
+    dbra %d0, rem_loop
+    
     move.l %a0, (wav_ptr)
-skip_wav:
-    move.l (%sp)+, %a0
+    
+    
+skip_load:
+    movem.l (%sp)+, %d0/%a0-%a2
+    
+    clr.b (wav_semaphore)
+    move.b #0, (0xC0001)     | LED off
+    
+wav_end:
     rte
     
     
 .bss
 wav_ptr: .space 4
 wav_end: .space 4
+wav_semaphore: .space 1
--- C/wav_player/wav_player.c
+++ C/wav_player/wav_player.c
@@ -5,34 +5,49 @@
 #include <io.h>
 #include <interrupts.h>
 
-extern uint32_t wav_ptr;
-extern uint32_t wav_end;
+extern volatile uint8_t *wav_ptr;
+extern volatile uint8_t *wav_end;
 extern void wav_isr();
 
 #define INTR_TIMR_B (1 << 0)
+#define INTR_GPI3 (1 << 3)
 
 int main() {
    // TIL311 = 0x01;
 
     serial_start(SERIAL_SAFE);
    // millis_start();
+  /* uint8_t c = 0;
+   while(1) {
+        TIL311 = c;
+        DELAY(20);
+        c++;
+    }*/
  
-    __vectors.user[MFP_INT + MFP_TIMERB - USER_ISR_START] = &wav_isr;
+    __vectors.user[MFP_INT + MFP_GPI3 - USER_ISR_START] = &wav_isr;
 
-    TBDR = 57;   // 16168.42105
+    TBDR = 29;   // approx 15.9 khz out
 	TBCR = 0x1;  // prescaler of 4
      
-	IERA |= INTR_TIMR_B;
-	IMRA |= INTR_TIMR_B;
+	AER |= (1 << 2);
+	IERB |= INTR_GPI3;
+	IMRB |= INTR_GPI3;
 	    
     wav_ptr = 0x4000;
-    wav_end = 0x80000 - 4096;
+    wav_end = wav_ptr + 428289;
     sei();
    
     DDR |= 1 << 1;
     
-    __asm volatile ("lb: move.b #2, (0xC0001)\n"
+    /*__asm volatile ("lb: move.b #2, (0xC0001)\n"
     					 "move.b #0, (0xC0001)\n"
-    					 "bra lb\n");		
+    					 "bra lb\n");		*/
+    for (int i = 0; i < 512; i++)
+        TIL311 = *wav_ptr++;	
+   // wav_isr();			 
+    while(1) {
+        if (wav_ptr == wav_end)
+            wav_ptr = 0x4000;
+    }
 	
 }
