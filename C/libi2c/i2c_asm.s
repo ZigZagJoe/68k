f57d81191f7027f602b7523b0a9bd662461f7725
zigzagjoe <zigzagjoe@gmail.com>
2015-05-27 22:28:25 -0500

    Superfast i2c! 2.17ms full read, .75ms partial.

--- C/libi2c/i2c_asm.s
+++ C/libi2c/i2c_asm.s
@@ -25,20 +25,53 @@
     btst #SDA, (%a0)    | GPDR
 .endm
 
+
+.macro SDA_INf
+    bclr %d1, (%a1)    | DDR
+.endm
+
+.macro SDA_OUTf
+    bset %d1, (%a1)    | DDR
+.endm
+
+.macro TST_SDAf
+    btst %d1, (%a0)    | GPDR
+.endm
+
+
 .macro SCL_HI  | alow SCL be pulled hi
     bclr #SCL, (%a1)    | DDR
 .endm
+
+.macro SCL_HIf  | alow SCL be pulled hi
+    bclr %d1, (%a1)    | DDR
+.endm
+
 
 .macro SDA_HI  | allow SDA be pulled hi
     bclr #SDA, (%a1)    | DDR
 .endm
 
+.macro SDA_HIf  | allow SDA be pulled hi
+    bclr %d1, (%a1)    | DDR
+.endm
+
+
 .macro SCL_LO  | force SCL low
     bset #SCL, (%a1)    | DDR
 .endm
 
-.macro SDA_LO  | force SDA low
+.macro SCL_LOf   | force SCL low
+    bset %d1, (%a1)    | DDR
+.endm
+
+
+.macro SDA_LO   | force SDA low
     bset #SDA, (%a1)    | DDR
+.endm
+
+.macro SDA_LOf  | force SDA low
+    bset %d1, (%a1)    | DDR
 .endm
 
 .macro I2C_START 
@@ -54,14 +87,113 @@
 | set SDA according to carry bit
 .macro SDA_OUTCC
     jcc 1f
-    SDA_HI
+    SDA_HIf
     jbra 2f
 1:
-    SDA_LO
+    SDA_LOf
 2:
 
     SCL_HI
     SCL_LO
+.endm
+
+.macro WRITE_BYTE
+    lsl.b #1, %d0
+    SDA_OUTCC
+    lsl.b #1, %d0
+    SDA_OUTCC
+    lsl.b #1, %d0
+    SDA_OUTCC
+    lsl.b #1, %d0
+    SDA_OUTCC
+    lsl.b #1, %d0
+    SDA_OUTCC
+    lsl.b #1, %d0
+    SDA_OUTCC
+    lsl.b #1, %d0
+    SDA_OUTCC
+    lsl.b #1, %d0
+    SDA_OUTCC
+
+    SDA_INf
+    
+    SCL_HI
+    TST_SDAf
+    jne 9f
+    SCL_LO
+    
+    SDA_LOf
+    SDA_OUTf
+.endm
+
+.macro READ_BYTE
+    clr.b %d0
+        
+    SDA_IN
+    
+    SCL_HIf
+    TST_SDA
+    jeq 1f 
+    ori.b #0b10000000, %d0   
+1:
+    SCL_LOf
+    SCL_HIf
+    TST_SDA
+    jeq 1f   
+    ori.b #0b01000000, %d0   
+1:
+    SCL_LOf
+    SCL_HIf
+    TST_SDA
+    jeq 1f  
+    ori.b #0b00100000, %d0   
+1:
+    SCL_LOf
+    SCL_HIf
+    TST_SDA
+    jeq 1f   
+    ori.b #0b00010000, %d0   
+1:
+    SCL_LOf
+    SCL_HIf
+    TST_SDA
+    jeq 1f    
+    ori.b #0b00001000, %d0   
+1:
+    SCL_LOf
+    SCL_HIf
+    TST_SDA
+    jeq 1f    
+    ori.b #0b00000100, %d0   
+1:
+    SCL_LOf
+    SCL_HIf
+    TST_SDA
+    jeq 1f    
+    ori.b #0b00000010, %d0   
+1:
+    SCL_LOf
+    SCL_HIf
+    TST_SDA
+    jeq 1f   
+    ori.b #0b00000001, %d0   
+1:
+    SCL_LOf
+
+    SDA_OUT
+.endm
+
+.macro SEND_ACK
+    SDA_LO
+    SCL_HIf
+    SCL_LOf
+    SDA_HI
+.endm
+
+.macro SEND_NACK    
+    SDA_HI
+    SCL_HIf
+    SCL_LOf
 .endm
 
 /*
@@ -77,79 +209,19 @@ i2c_read_byte:
     | clear bits in GPDR, so dont need to unset them during run
     andi.b #0b00111111, (%a0)
 
-    move.b (7,%sp), %d1
+    moveq #SCL, %d1   | keep SCL in %d1 to speed up clock cycles
     
-_i2c_read_byte:       | for internal use only 
-    clr.l %d0
-    SDA_IN
+    READ_BYTE
+  
+    tst.b (7,%sp)     | send ACK or NACK?
+    jne 2f
     
-    SCL_HI
-    TST_SDA
-    jeq 1f    
-    bset.b #7, %d0
-1:
-    SCL_LO
-    SCL_HI
-    TST_SDA
-    jeq 1f   
-    bset.b #6, %d0
-1:
-    SCL_LO
-    SCL_HI
-    TST_SDA
-    jeq 1f  
-    bset.b #5, %d0
-1:
-    SCL_LO
-    SCL_HI
-    TST_SDA
-    jeq 1f   
-    bset.b #4, %d0
-1:
-    SCL_LO
-    SCL_HI
-    TST_SDA
-    jeq 1f    
-    bset.b #3, %d0
-1:
-    SCL_LO
-    SCL_HI
-    TST_SDA
-    jeq 1f    
-    bset.b #2, %d0
-1:
-    SCL_LO
-    SCL_HI
-    TST_SDA
-    jeq 1f    
-    bset.b #1, %d0
-1:
-    SCL_LO
-    SCL_HI
-    TST_SDA
-    jeq 1f   
-    bset.b #0, %d0
-1:
-    SCL_LO
-
-    SDA_OUT
+    SEND_ACK
+    jbra 1f
     
-    tst.b %d1
-    jne _nack
-    
-    SDA_LO
-    SCL_HI
-    SCL_LO
-    SDA_HI
-    jbra end
-    
-_nack:
-   
-    SDA_HI
-    SCL_HI
-    SCL_LO
-    
-end:
+2:
+    SEND_NACK
+1:
     rts
     
 /* uint8_t i2c_write_byte(uint8_t byte);
@@ -164,40 +236,15 @@ i2c_write_byte:
     | clear bits in GPDR, so dont need to unset them during run
     andi.b #0b00111111, (%a0)
   
-    move.b (7,%sp), %d1
+    move.b (7,%sp), %d0
     
-_i2c_write_byte:       | for internal use only 
-    lsl.b #1, %d1
-    SDA_OUTCC
-    lsl.b #1, %d1
-    SDA_OUTCC
-    lsl.b #1, %d1
-    SDA_OUTCC
-    lsl.b #1, %d1
-    SDA_OUTCC
-    lsl.b #1, %d1
-    SDA_OUTCC
-    lsl.b #1, %d1
-    SDA_OUTCC
-    lsl.b #1, %d1
-    SDA_OUTCC
-    lsl.b #1, %d1
-    SDA_OUTCC
-
-    SDA_IN
-    
-    SCL_HI
-    TST_SDA
-    jne rec_nak
-    SCL_LO
-    
-    SDA_LO
-    SDA_OUT
+    moveq #SDA, %d1    | keep SDA in %d1 to speed up data bits
+    WRITE_BYTE
     
     moveq #1, %d0
     rts
     
-rec_nak:
+9:                     | nack, xmit failed
     moveq #0, %d0
     rts
     
@@ -206,64 +253,59 @@ arguments: buffer to write to, start register, num bytes
 returns: bool success
 */
 i2c_reg_read: 
-	movm.l %a2-%a3/%d2,-(%sp)
+	movm.l %a2/%d2,-(%sp)
 
     move.l #GPDR, %a0
     move.l #DDR, %a1
     
-	andi.b #0b00111111, (%a0)   | clear bits in GPDR so routines dont need to
+	andi.b #0b00111111, (%a0)   | clear bits in GPDR so subroutines do not have to
+    moveq #SDA, %d1             | keep SDA in %d1 to speed up bits out for writes
     
-	lea _i2c_write_byte, %a2
-	
 	I2C_START
 	
 	/* LSB 0 = write */
     move.b (curr_slave), %d2
-    move.b %d2, %d1             | current slave address
-    jbsr (%a2)
-	jbeq _return_failed
+    move.b %d2, %d0             | current slave address
+    WRITE_BYTE
 
-	move.b (11+12,%sp), %d1     | start reg
-	jbsr (%a2)
-	jbeq _return_failed
+	move.b (11+8,%sp), %d0      | start reg
+	WRITE_BYTE
 	
 	I2C_STOP
 	I2C_START
 	
-    move.b %d2, %d1             | current slave address
-    ori.b #1, %d1               | set LSB = read
-    jbsr (%a2)
-	jbeq _return_failed
+    move.b %d2, %d0             | current slave address
+    ori.b #1, %d0               | set LSB = read
+    WRITE_BYTE
 	
-	move.w (14+12,%sp),%d2      | count of bytes to move
-	move.l ( 4+12,%sp),%a3      | destination
+	move.w (14+8,%sp),%d2       | count of bytes to move
+	move.l ( 4+8,%sp),%a2       | destination
 	
 	/* a3 = dest
 	   d2 = count */
 	   
-	subq.w #2, %d2              | gotta send NACK on the last byte
-	
-    lea _i2c_read_byte, %a2
-	
-	moveq #0, %d1               | send ack 
-	
-_read_loop:
-	jbsr (%a2)
-    move.b %d0, (%a3)+
+	subq.w #2, %d2              | must send NACK on the last byte
+	moveq #SCL, %d1             | keep SCL in %d1 to speed up clock cycles
+    
+_read_loop:                     | send count-1 bytes with acks
+	READ_BYTE                   | send byte with ack
+	SEND_ACK
+    move.b %d0, (%a2)+
     
 	dbra %d2, _read_loop
 	
-	moveq #1, %d1               | send nack
-	jbsr (%a2)
-    move.b %d0, (%a3)+
+	READ_BYTE                   | send final byte with nack
+	SEND_NACK
+    move.b %d0, (%a2)+
     
 	I2C_STOP
 	
 	moveq #1, %d0
 _r_exit:
-	movm.l (%sp)+, %a2-%a3/%d2
+	movm.l (%sp)+, %a2/%d2
 	rts
 
+9:
 _return_failed:
 	moveq #0,%d0
 	jbra _r_exit
