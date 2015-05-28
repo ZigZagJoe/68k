a6063adfe2db3ef8d53cb082f79329af9e09cbbf
zigzagjoe <zigzagjoe@gmail.com>
2015-05-27 23:16:13 -0500

    Testbed testing stuff

--- C/i2c_testbed/i2c_test.c
+++ C/i2c_testbed/i2c_test.c
@@ -14,13 +14,20 @@
 
 void partial_read(){
     int16_t gyro_y = 0;
+    int y_dps;
+    
      while(true) {
-        bset_a(GPDR, 1);
-        int code = i2c_reg_read(&gyro_y, GYRO_ZOUT_H, 2); 
+
+       bset_a(GPDR, 1);
+        i2c_reg_read(&gyro_y, GYRO_ZOUT_H, 2); 
         bclr_a(GPDR, 1);
-         
-        int y_dps = (gyro_y >> 7) - (gyro_y >> 12) + (gyro_y >> 14); 
+        
+        
+        y_dps = gyro_y / 131; 
+     
+       // putc(y_dps);
         printf("%5d\n", y_dps);
+        serial_wait();
      }
 }
 
@@ -34,16 +41,19 @@ void full_read() {
         bclr_a(GPDR, 1);
         
         printf("AcX = %5d  AcY = %5d  AcZ = %5d TMP = %5d GyX = %5d  GyY = %5d  GyZ = %5d\n",all_regs[0],all_regs[1],all_regs[2],tmp_raw_to_F(all_regs[3]),all_regs[4],all_regs[5],all_regs[6]);
+        serial_wait();
     }
 }    
+
+// 2.66ms
     
-int16_t raw_to_250dps(int16_t x) {
+/*int16_t raw_to_250dps(int16_t x) {
    return (x >> 7) - (x >> 12) + (x >> 14); 
 }
 
 int16_t integrate7ms(int16_t x) {
     return  (x >> 7) - (x >> 10) + (x >> 13) + (x >> 15);
-}
+}*/
 
 #define DEG_TO_RAW 131
 
@@ -53,22 +63,26 @@ void integrate() {
     
     uint8_t i = 0;
     while(true) {
+        bset_a(GPDR, 1);
         DELAY_MS(6);
         DELAY(20);
         bset_a(GPDR, 1);
         bclr_a(GPDR, 1);
         i2c_reg_read(&gyro_y, GYRO_ZOUT_H, 2); 
-        acc+=integrate7ms(gyro_y);
+        
+       // acc += integrate7ms(gyro_y);
+        acc += gyro_y / 143;
     
         i++;
         if (i == 60) {
             i = 0;
-            printf("%d\n",raw_to_250dps(acc));
+            printf("%d\n",acc/131);
         }
+        bclr_a(GPDR, 1);
     }
 
 }
-    
+ 
 //void lzfx_decompress(int a,int b, int c, int d) {}
 int main() {
    // TIL311 = 0x01;
@@ -76,8 +90,11 @@ int main() {
     serial_start(SERIAL_SAFE);
     //millis_start();
     sei();
-    
+
     bset(DDR,1);
+    bclr_a(GPDR,1);    
+    
+    DELAY_MS(10);
     
     uint8_t success = 0; 
 
@@ -96,9 +113,9 @@ int main() {
     //DELAY_MS(200);  // Delay a while to let the device execute the self-test
     printf("Begin loop\n");
 
-    //partial_read();
-    //full_read();
-    integrate();
+   // partial_read();
+    full_read();
+    //integrate();
 }
 
 void leave_critical() {
--- C/testbed/c_test.c
+++ C/testbed/c_test.c
@@ -10,89 +10,10 @@
 #include <time.h>
 #include <kernel.h>
 
-void task_su() {
-    while(true) {
-        cli();
-        sei();
-        
-        for (int i = 0; i < 255; i++) {
-            putc(i);
-            // super task must always yield manually
-            supertask_yield();
-        }
-    
-    }
-    // super task must always exit manually
-    exit_task();
-}
-
 
 //void lzfx_decompress(int a,int b, int c, int d) {}
 int main() {
    // TIL311 = 0x01;
 
-    serial_start(SERIAL_SAFE);
-    //millis_start();
-    //sei();
     
-    
-    bset (DDR, 6);
-    
-    
-    printf("creating task\n");
-    task_t su = create_task(&task_be_boss,0);
-    task_struct_t *ptr = su >> 16;
-    ptr->FLAGS |= (1<<13);
-    
-    // 1ms = 169        90
-    // 252 = center
-    // 2ms = 169 * 2    -90
-   /* while(1) {
-        bset(GPDR,6);
-        DELAY(252);
-        bclr(GPDR,6);
-        DELAY_MS(20);
-    }*/
-    
-    
-    
-   /* TCDCR |= 0x3 << 4;
-    // 220 = max travel one way
-
-    int8_t dir = 1;
-    uint8_t num = 110;
-    
-    while(true) {
-        num += dir;
-        TCDR = num;
-        
-        DELAY_MS(20);
-        
-        if (num >= 220) {
-            dir = -1;
-        } else if (num <= 110)
-            dir = 1;
-    }
-    
-    uint32_t start, end;
-    
-    lcd_init();
-	
-	end = millis();
-    while(true) {
-       // lcd_cursor(0,0);
-       // lcd_printf("Runtime: %d.%03d    ",millis()/1000, (millis()%1000));
-        start = millis();
-        if (start < end) 
-            printf("Warning backtick detected: %d vs %d\n",start, end);
-            
-        end = start;
-       // printf("%d\n",end);
-    }
-
-    start = millis();
-    mem_dump(0x1000,27);
-    end = millis();
-    
-     */   
 }
