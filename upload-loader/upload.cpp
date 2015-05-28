1e5e730257f70c059604eb39b60a1084f4b01f06
zigzagjoe <zigzagjoe@gmail.com>
2015-05-27 23:16:33 -0500

    Allow tabs in terminal

--- upload-loader/upload.cpp
+++ upload-loader/upload.cpp
@@ -997,7 +997,7 @@ void monitor(int fd) {
             serputc(fd,getchar());
         
         while (1 == read(fd, &ch, 1)) {
-            if (isprint(ch) || ch == '\n') {
+            if (isprint(ch) || ch == '\n' || ch == '\t') {
                 putchar(ch);
             } else {
                // printf("ʘ");
