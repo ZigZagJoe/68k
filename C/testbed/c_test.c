a851d6c861e1970aa2a7a86d868650d3718cf465
zigzagjoe <zigzagjoe@gmail.com>
2015-06-20 17:58:08 -0500

    Delay testing

--- C/testbed/c_test.c
+++ C/testbed/c_test.c
@@ -14,6 +14,18 @@
 //void lzfx_decompress(int a,int b, int c, int d) {}
 int main() {
    // TIL311 = 0x01;
+    serial_start(SERIAL_SAFE);
+    sei();
 
+    DDR |= 2;
+    while(true) {
+        bset_a(GPDR, 1);
+        DELAY_US(40);
+        bclr_a(GPDR, 1);
+        DELAY_US(40);
+    }
     
 }
+
+void enter_critical() {}
+void leave_critical() {}
